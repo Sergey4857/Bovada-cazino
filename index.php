7f7fd91824953208b1dcea1f2ae73f8bb72e760f
Sergey4857 <yasak.sergey@gmail.com>
2025-07-27 17:01:36 +0300

    styles

--- index.php
+++ index.php
@@ -22,14 +22,44 @@
     <div class="container hero_cotent">
       <h1>get ready to start playing with <span class="highlight">$3000 welcome bonus</span></h1>
       <p><span>100%</span> match bonus up to <span>$1000</span> on first <span>3 deposits</span></p>
-      <div class="hero_promo">
 
-        <div>First Deposit</div>
-        <div class="deposit_promo">cawelcome100</div>
-        <button class="copy_btn">copy</button>
+      <div class="hero_promo">
+        <div class="hero_first_deposit">
+          <div class="hero_subtitle">First Deposit</div>
+          <div class="deposit_wrap">
+            <div class="deposit_promo">cawelcome100</div>
+            <button class="copy_btn" type="button">copy</button>
+          </div>
+        </div>
+
+        <div class="hero_second_deposit">
+          <div class="hero_subtitle">Second and Third Deposit</div>
+          <div class="deposit_wrap">
+            <div class="deposit_promo">BV2NDCWB</div>
+            <button class="copy_btn" type="button">copy</button>
+          </div>
+        </div>
       </div>
+
+
       <a class="hero_btn_link" href="/">claim your bonus</a>
     </div>
+
+    <p class="description"><span>Bovada</span> is licensed and regulated by the Union of the Comoros and the Central
+      Reserve Authority of
+      Western Sahara.</p>
+
+    <div class="payment_images">
+      <img src="/src/icons/visa.svg" alt="visa">
+      <img src="/src/icons/mastercard.svg" alt="mastercard">
+      <img src="/src/icons/L.svg" alt="ethereum">
+      <img src="/src/icons/t.svg" alt="bitcoin">
+      <img src="/src/icons/Bitcoin.svg" alt="litecoin">
+      <img src="/src/icons/Bitcoin_green.svg" alt="litecoin">
+    </div>
+
+
+
   </section>
 
 
